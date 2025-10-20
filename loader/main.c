5dcb99333e983dba94d55cbd23d45a5834327ed1
Rinnegatamante <rinnegatamante@gmail.com>
2025-10-20 23:45:35 +0200

    Fix setdetachstate impl and compilation with latest SDL2.

--- loader/main.c
+++ loader/main.c
@@ -171,7 +171,7 @@ int ret1(void) {
 	return 1;
 }
 
-#define  MUTEX_TYPE_NORMAL     0x0000
+#define  MUTEX_TYPE_NORMAL	 0x0000
 #define  MUTEX_TYPE_RECURSIVE  0x4000
 #define  MUTEX_TYPE_ERRORCHECK 0x8000
 
@@ -179,249 +179,249 @@ static pthread_t s_pthreadSelfRet;
 
 static void init_static_mutex(pthread_mutex_t **mutex)
 {
-    pthread_mutex_t *mtxMem = NULL;
+	pthread_mutex_t *mtxMem = NULL;
 
-    switch ((int)*mutex) {
+	switch ((int)*mutex) {
 	case MUTEX_TYPE_NORMAL: {
-	    pthread_mutex_t initTmpNormal = PTHREAD_MUTEX_INITIALIZER;
-	    mtxMem = calloc(1, sizeof(pthread_mutex_t));
-	    sceClibMemcpy(mtxMem, &initTmpNormal, sizeof(pthread_mutex_t));
-	    *mutex = mtxMem;
-	    break;
+		pthread_mutex_t initTmpNormal = PTHREAD_MUTEX_INITIALIZER;
+		mtxMem = calloc(1, sizeof(pthread_mutex_t));
+		sceClibMemcpy(mtxMem, &initTmpNormal, sizeof(pthread_mutex_t));
+		*mutex = mtxMem;
+		break;
 	}
 	case MUTEX_TYPE_RECURSIVE: {
-	    pthread_mutex_t initTmpRec = PTHREAD_RECURSIVE_MUTEX_INITIALIZER;
-	    mtxMem = calloc(1, sizeof(pthread_mutex_t));
-	    sceClibMemcpy(mtxMem, &initTmpRec, sizeof(pthread_mutex_t));
-	    *mutex = mtxMem;
-	    break;
+		pthread_mutex_t initTmpRec = PTHREAD_RECURSIVE_MUTEX_INITIALIZER;
+		mtxMem = calloc(1, sizeof(pthread_mutex_t));
+		sceClibMemcpy(mtxMem, &initTmpRec, sizeof(pthread_mutex_t));
+		*mutex = mtxMem;
+		break;
 	}
 	case MUTEX_TYPE_ERRORCHECK: {
-	    pthread_mutex_t initTmpErr = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER;
-	    mtxMem = calloc(1, sizeof(pthread_mutex_t));
-	    sceClibMemcpy(mtxMem, &initTmpErr, sizeof(pthread_mutex_t));
-	    *mutex = mtxMem;
-	    break;
+		pthread_mutex_t initTmpErr = PTHREAD_ERRORCHECK_MUTEX_INITIALIZER;
+		mtxMem = calloc(1, sizeof(pthread_mutex_t));
+		sceClibMemcpy(mtxMem, &initTmpErr, sizeof(pthread_mutex_t));
+		*mutex = mtxMem;
+		break;
 	}
 	default:
-	    break;
-    }
+		break;
+	}
 }
 
 static void init_static_cond(pthread_cond_t **cond)
 {
-    if (*cond == NULL) {
-	pthread_cond_t initTmp = PTHREAD_COND_INITIALIZER;
-	pthread_cond_t *condMem = calloc(1, sizeof(pthread_cond_t));
-	sceClibMemcpy(condMem, &initTmp, sizeof(pthread_cond_t));
-	*cond = condMem;
-    }
+	if (*cond == NULL) {
+		pthread_cond_t initTmp = PTHREAD_COND_INITIALIZER;
+		pthread_cond_t *condMem = calloc(1, sizeof(pthread_cond_t));
+		sceClibMemcpy(condMem, &initTmp, sizeof(pthread_cond_t));
+		*cond = condMem;
+	}
 }
 
 int pthread_attr_destroy_soloader(pthread_attr_t **attr)
 {
-    int ret = pthread_attr_destroy(*attr);
-    free(*attr);
-    return ret;
+	int ret = pthread_attr_destroy(*attr);
+	free(*attr);
+	return ret;
 }
 
 int pthread_attr_getstack_soloader(const pthread_attr_t **attr,
 				   void **stackaddr,
 				   size_t *stacksize)
 {
-    return pthread_attr_getstack(*attr, stackaddr, stacksize);
+	return pthread_attr_getstack(*attr, stackaddr, stacksize);
 }
 
 __attribute__((unused)) int pthread_condattr_init_soloader(pthread_condattr_t **attr)
 {
-    *attr = calloc(1, sizeof(pthread_condattr_t));
+	*attr = calloc(1, sizeof(pthread_condattr_t));
 
-    return pthread_condattr_init(*attr);
+	return pthread_condattr_init(*attr);
 }
 
 __attribute__((unused)) int pthread_condattr_destroy_soloader(pthread_condattr_t **attr)
 {
-    int ret = pthread_condattr_destroy(*attr);
-    free(*attr);
-    return ret;
+	int ret = pthread_condattr_destroy(*attr);
+	free(*attr);
+	return ret;
 }
 
 int pthread_cond_init_soloader(pthread_cond_t **cond,
-			       const pthread_condattr_t **attr)
+				   const pthread_condattr_t **attr)
 {
-    *cond = calloc(1, sizeof(pthread_cond_t));
+	*cond = calloc(1, sizeof(pthread_cond_t));
 
-    if (attr != NULL)
-	return pthread_cond_init(*cond, *attr);
-    else
-	return pthread_cond_init(*cond, NULL);
+	if (attr != NULL)
+		return pthread_cond_init(*cond, *attr);
+	else
+		return pthread_cond_init(*cond, NULL);
 }
 
 int pthread_cond_destroy_soloader(pthread_cond_t **cond)
 {
-    int ret = pthread_cond_destroy(*cond);
-    free(*cond);
-    return ret;
+	int ret = pthread_cond_destroy(*cond);
+	free(*cond);
+	return ret;
 }
 
 int pthread_cond_signal_soloader(pthread_cond_t **cond)
 {
-    init_static_cond(cond);
-    return pthread_cond_signal(*cond);
+	init_static_cond(cond);
+	return pthread_cond_signal(*cond);
 }
 
 int pthread_cond_timedwait_soloader(pthread_cond_t **cond,
-				    pthread_mutex_t **mutex,
-				    struct timespec *abstime)
+					pthread_mutex_t **mutex,
+					struct timespec *abstime)
 {
-    init_static_cond(cond);
-    init_static_mutex(mutex);
-    return pthread_cond_timedwait(*cond, *mutex, abstime);
+	init_static_cond(cond);
+	init_static_mutex(mutex);
+	return pthread_cond_timedwait(*cond, *mutex, abstime);
 }
 
 int pthread_create_soloader(pthread_t **thread,
-			    const pthread_attr_t **attr,
-			    void *(*start)(void *),
-			    void *param)
+				const pthread_attr_t **attr,
+				void *(*start)(void *),
+				void *param)
 {
-    *thread = calloc(1, sizeof(pthread_t));
+	*thread = calloc(1, sizeof(pthread_t));
 
-    if (attr != NULL) {
-	pthread_attr_setstacksize(*attr, 512 * 1024);
-	return pthread_create(*thread, *attr, start, param);
-    } else {
-	pthread_attr_t attrr;
-	pthread_attr_init(&attrr);
-	pthread_attr_setstacksize(&attrr, 512 * 1024);
-	return pthread_create(*thread, &attrr, start, param);
-    }
+	if (attr != NULL) {
+		pthread_attr_setstacksize(*attr, 512 * 1024);
+		return pthread_create(*thread, *attr, start, param);
+	} else {
+		pthread_attr_t attrr;
+		pthread_attr_init(&attrr);
+		pthread_attr_setstacksize(&attrr, 512 * 1024);
+		return pthread_create(*thread, &attrr, start, param);
+	}
 
 }
 
 int pthread_mutexattr_init_soloader(pthread_mutexattr_t **attr)
 {
-    *attr = calloc(1, sizeof(pthread_mutexattr_t));
+	*attr = calloc(1, sizeof(pthread_mutexattr_t));
 
-    return pthread_mutexattr_init(*attr);
+	return pthread_mutexattr_init(*attr);
 }
 
 int pthread_mutexattr_settype_soloader(pthread_mutexattr_t **attr, int type)
 {
-    return pthread_mutexattr_settype(*attr, type);
+	return pthread_mutexattr_settype(*attr, type);
 }
 
 int pthread_mutexattr_setpshared_soloader(pthread_mutexattr_t **attr, int pshared)
 {
-    return pthread_mutexattr_setpshared(*attr, pshared);
+	return pthread_mutexattr_setpshared(*attr, pshared);
 }
 
 int pthread_mutexattr_destroy_soloader(pthread_mutexattr_t **attr)
 {
-    int ret = pthread_mutexattr_destroy(*attr);
-    free(*attr);
-    return ret;
+	int ret = pthread_mutexattr_destroy(*attr);
+	free(*attr);
+	return ret;
 }
 
 int pthread_mutex_destroy_soloader(pthread_mutex_t **mutex)
 {
-    int ret = pthread_mutex_destroy(*mutex);
-    free(*mutex);
-    return ret;
+	int ret = pthread_mutex_destroy(*mutex);
+	free(*mutex);
+	return ret;
 }
 
 int pthread_mutex_init_soloader(pthread_mutex_t **mutex,
 				const pthread_mutexattr_t **attr)
 {
-    *mutex = calloc(1, sizeof(pthread_mutex_t));
+	*mutex = calloc(1, sizeof(pthread_mutex_t));
 
-    if (attr != NULL)
-	return pthread_mutex_init(*mutex, *attr);
-    else
-	return pthread_mutex_init(*mutex, NULL);
+	if (attr != NULL)
+		return pthread_mutex_init(*mutex, *attr);
+	else
+		return pthread_mutex_init(*mutex, NULL);
 }
 
 int pthread_mutex_lock_soloader(pthread_mutex_t **mutex)
 {
-    init_static_mutex(mutex);
-    return pthread_mutex_lock(*mutex);
+	init_static_mutex(mutex);
+	return pthread_mutex_lock(*mutex);
 }
 
 int pthread_mutex_trylock_soloader(pthread_mutex_t **mutex)
 {
-    init_static_mutex(mutex);
-    return pthread_mutex_trylock(*mutex);
+	init_static_mutex(mutex);
+	return pthread_mutex_trylock(*mutex);
 }
 
 int pthread_mutex_unlock_soloader(pthread_mutex_t **mutex)
 {
-    return pthread_mutex_unlock(*mutex);
+	return pthread_mutex_unlock(*mutex);
 }
 
 int pthread_join_soloader(const pthread_t *thread, void **value_ptr)
 {
-    return pthread_join(*thread, value_ptr);
+	return pthread_join(*thread, value_ptr);
 }
 
 int pthread_cond_wait_soloader(pthread_cond_t **cond, pthread_mutex_t **mutex)
 {
-    return pthread_cond_wait(*cond, *mutex);
+	return pthread_cond_wait(*cond, *mutex);
 }
 
 int pthread_cond_broadcast_soloader(pthread_cond_t **cond)
 {
-    return pthread_cond_broadcast(*cond);
+	return pthread_cond_broadcast(*cond);
 }
 
 int pthread_attr_init_soloader(pthread_attr_t **attr)
 {
-    *attr = calloc(1, sizeof(pthread_attr_t));
+	*attr = calloc(1, sizeof(pthread_attr_t));
 
-    return pthread_attr_init(*attr);
+	return pthread_attr_init(*attr);
 }
 
 int pthread_attr_setdetachstate_soloader(pthread_attr_t **attr, int state)
 {
-    return pthread_attr_setdetachstate(*attr, state);
+	return pthread_attr_setdetachstate(*attr, !state);
 }
 
 int pthread_attr_setstacksize_soloader(pthread_attr_t **attr, size_t stacksize)
 {
-    return pthread_attr_setstacksize(*attr, stacksize);
+	return pthread_attr_setstacksize(*attr, stacksize);
 }
 
 int pthread_attr_setschedparam_soloader(pthread_attr_t **attr,
 					const struct sched_param *param)
 {
-    return pthread_attr_setschedparam(*attr, param);
+	return pthread_attr_setschedparam(*attr, param);
 }
 
 int pthread_attr_setstack_soloader(pthread_attr_t **attr,
 				   void *stackaddr,
 				   size_t stacksize)
 {
-    return pthread_attr_setstack(*attr, stackaddr, stacksize);
+	return pthread_attr_setstack(*attr, stackaddr, stacksize);
 }
 
 int pthread_setschedparam_soloader(const pthread_t *thread, int policy,
 				   const struct sched_param *param)
 {
-    return pthread_setschedparam(*thread, policy, param);
+	return pthread_setschedparam(*thread, policy, param);
 }
 
 int pthread_getschedparam_soloader(const pthread_t *thread, int *policy,
 				   struct sched_param *param)
 {
-    return pthread_getschedparam(*thread, policy, param);
+	return pthread_getschedparam(*thread, policy, param);
 }
 
 int pthread_detach_soloader(const pthread_t *thread)
 {
-    return pthread_detach(*thread);
+	return pthread_detach(*thread);
 }
 
 int pthread_getattr_np_soloader(pthread_t* thread, pthread_attr_t *attr) {
-    fprintf(stderr, "[WARNING!] Not implemented: pthread_getattr_np\n");
-    return 0;
+	fprintf(stderr, "[WARNING!] Not implemented: pthread_getattr_np\n");
+	return 0;
 }
 
 int pthread_equal_soloader(const pthread_t *t1, const pthread_t *t2)
@@ -430,13 +430,13 @@ int pthread_equal_soloader(const pthread_t *t1, const pthread_t *t2)
 		return 1;
 	if (!t1 || !t2)
 		return 0;
-    return pthread_equal(*t1, *t2);
+	return pthread_equal(*t1, *t2);
 }
 
 pthread_t *pthread_self_soloader()
 {
-    s_pthreadSelfRet = pthread_self();
-    return &s_pthreadSelfRet;
+	s_pthreadSelfRet = pthread_self();
+	return &s_pthreadSelfRet;
 }
 
 #ifndef MAX_TASK_COMM_LEN
@@ -444,18 +444,18 @@ pthread_t *pthread_self_soloader()
 #endif
 
 int pthread_setname_np_soloader(const pthread_t *thread, const char* thread_name) {
-    if (thread == 0 || thread_name == NULL) {
+	if (thread == 0 || thread_name == NULL) {
 	return EINVAL;
-    }
-    size_t thread_name_len = strlen(thread_name);
-    if (thread_name_len >= MAX_TASK_COMM_LEN) {
+	}
+	size_t thread_name_len = strlen(thread_name);
+	if (thread_name_len >= MAX_TASK_COMM_LEN) {
 	return ERANGE;
-    }
+	}
 
-    // TODO: Implement the actual name setting if possible
-    fprintf(stderr, "PTHR: pthread_setname_np with name %s\n", thread_name);
+	// TODO: Implement the actual name setting if possible
+	fprintf(stderr, "PTHR: pthread_setname_np with name %s\n", thread_name);
 
-    return 0;
+	return 0;
 }
 
 int clock_gettime_hook(int clk_id, struct timespec *t) {
